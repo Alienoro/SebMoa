01a43f12688733eb57f20ea76fd50c6fb31ab205
rutmoaj <rutmoaj6gmail.com>
2025-10-21 15:23:07 +0200

    moa wrote something

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+	System.print.outln("Moa was here");
 	System.out.println("Hello from Person A and Person B!");
 	System.out.println("Hej hej test :)";
     }
